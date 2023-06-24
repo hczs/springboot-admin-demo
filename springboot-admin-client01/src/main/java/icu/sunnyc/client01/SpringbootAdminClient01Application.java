816ac768914130fa03e7ed176a887d4e0eaaf1a8
sunny <hczshd@163.com>
2023-06-24 10:58:24 +0800

    Initial commit

---
+++ springboot-admin-client01/src/main/java/icu/sunnyc/client01/SpringbootAdminClient01Application.java
@@ -0,0 +1,13 @@
+package icu.sunnyc.client01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAdminClient01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAdminClient01Application.class, args);
+    }
+
+}
---
+++ springboot-admin-client01/src/test/java/icu/sunnyc/client01/SpringbootAdminClient01ApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.sunnyc.client01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdminClient01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
